c1f71dfeeae8fa2a97a7b48fd5358e00fb8bdc6b
Loraine Kanervisto <loraine@lorainekv.com>
2020-09-19 22:17:20 -0700

    Introspection

--- greeting.rb
+++ greeting.rb
@@ -8,5 +8,13 @@ class Greeting
   end
 end
 
-greeting = Greeting.new("Hi!")
-puts greeting.welcome
+my_object = Greeting.new("Hi!")
+puts my_object.welcome
+
+# Introspection
+puts "The class of my_object is #{my_object.class}."
+
+# Passing "false" to the instance_methods method omits the methods inherited from ancestors
+puts "The instance methods of my_object are #{my_object.class.instance_methods(false)}"
+
+puts "The instance variables of my_object are #{my_object.instance_variables}"
